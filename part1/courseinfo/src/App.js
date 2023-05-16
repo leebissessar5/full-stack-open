155c1033a07e5decaea6ff795af68647d1146745
leebissessar5 <leebissessar@gmail.com>
2023-05-16 11:50:27 -0400

    complete ex1.2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -4,12 +4,18 @@ const Header = (props) => (
   </>
 )
 
-const Content = (props) => (
+const Part = (props) => (
   <>
-    <p>{props.part1} {props.exercises1}</p>
-    <p>{props.part2} {props.exercises2}</p>
-    <p>{props.part3} {props.exercises3}</p>
+    <p>{props.part} {props.exercises}</p>
   </>
+)
+
+const Content = (props) => (
+  <div>
+    <Part part={props.part1} exercises={props.exercises1} />
+    <Part part={props.part2} exercises={props.exercises2} />
+    <Part part={props.part3} exercises={props.exercises3} />
+  </div>
 )
 
 const Total = (props) => (
