4973b13ba8679e086bbc8bcb5701307685a8482f
leebissessar5 <leebissessar@gmail.com>
2023-05-16 11:45:44 -0400

    complete ex1.1

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,3 +1,23 @@
+const Header = (props) => (
+  <>
+    <h1>{props.name}</h1>
+  </>
+)
+
+const Content = (props) => (
+  <>
+    <p>{props.part1} {props.exercises1}</p>
+    <p>{props.part2} {props.exercises2}</p>
+    <p>{props.part3} {props.exercises3}</p>
+  </>
+)
+
+const Total = (props) => (
+  <>
+    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+  </>
+)
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -8,19 +28,13 @@ const App = () => {
   const exercises3 = 14
 
   return (
-    <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-    </div>
+    <>
+      <Header name={course} />
+      <Content part1={part1} exercises1={exercises1}
+      part2={part2} exercises2={exercises2}
+      part3={part3} exercises3={part3} />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+    </>
   )
 }
 
