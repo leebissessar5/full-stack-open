7eb1f4ed408928338fe87bcb5dbe308102d7fb9c
leebissessar5 <leebissessar@gmail.com>
2023-05-20 22:02:31 -0400

    cleanup and complete ex2.5

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,39 +1,4 @@
-const Header = (props) => {
-  return <h1>{props.course}</h1>
-}
-
-const Total = ({parts}) => {
-  const sumOfExercises = parts.reduce((sum, part) => sum + part.exercises, 0)
-
-  return <b>total of {sumOfExercises} exercises</b>;
-}
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.part} {props.exercises}
-    </p>
-  )
-}
-
-const Content = ({parts}) => {
-  return (
-    <div>
-      {parts.map(part => <Part key={part.id} part={part.name} exercises={part.exercises}/>)}
-    </div>
-  )
-}
-
-const Course = ({course}) => {
-  return (
-    <div>
-      <Header course={course.name}/>
-      <Content parts={course.parts}/>
-      <Total parts={course.parts} />
-    </div>
-    
-  )
-}
+import Course from "./components/Course"
 
 const App = () => {
   const courses = [
