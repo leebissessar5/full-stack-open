4446c38b0536d63ee5834ba0cf2e1c2f1552385e
leebissessar5 <leebissessar@gmail.com>
2023-05-26 05:35:18 -0400

    upload ex2.20

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,17 +1,46 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const CountryInfo = ({country}) => (
+const WeatherInfo = ({capital, weatherData}) => (
+  <>
+      <h2>Weather in {capital}</h2>
+      <p>temperature {weatherData.temperature} Celsius</p>
+      <img src={`https://openweathermap.org/img/wn/${weatherData.icon}@2x.png`} alt="openweather weather icon" />
+      <p>wind {weatherData.wind} m/s</p>
+  </>
+)
+
+const CountryInfo = ({country}) => {
+  const [weatherData, setWeatherData] = useState(null)
+
+  useEffect(() => {
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${country.capital}&appid=${process.env.REACT_APP_API_KEY}`)
+    .then(response => {
+      setWeatherData({
+        'temperature': response.data.main.temp,
+        'icon': response.data.weather[0].icon,
+        'wind': response.data.wind.speed
+      })
+    })
+  }, [country.capital])
+
+  const flagStyle = {
+      width: '10%'
+  }
+  
+  return (
   <div>
         <h1>{country.name}</h1>
         <p>capital {country.capital}</p>
         <p>area {country.area}</p>
+        <h3>languages:</h3>
         <ul>
           {country.languages.map(lang => <li key={lang}>{lang}</li>)}
         </ul>
-        <img className="flag" src={country.flags.svg} alt={country.flags.alt} />
+        <img style={flagStyle} src={country.flags.svg} alt={country.flags.alt} />
+        {weatherData && <WeatherInfo capital={country.capital} weatherData={weatherData} />}
   </div>
-)
+)}
 
 const Country = ({country, toggleDisplay}) => {
   const label = country.display? "hide" : "show"
@@ -73,7 +102,7 @@ function App() {
               'name': country.name.common,
               'capital': country.capital,
               'area': country.area,
-              'languages': Object.values(country.languages),
+              'languages': country.languages ? Object.values(country.languages) : [], // Check if country.languages is defined
               'flags': country.flags,
               'display': false
             }
