28bb32fc6919d9ad272750e4897b2038fda6ad11
leebissessar5 <leebissessar@gmail.com>
2023-05-27 05:25:52 -0400

    complete ex3.4

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-const persons = [
+let persons = [
     { 
       "id": 1,
       "name": "Arto Hellas", 
@@ -52,6 +52,13 @@ app.get("/api/persons/:id", (request, response) => {
   } else {
     response.status(404).end()
   }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
 })
 
 const PORT = 3001
