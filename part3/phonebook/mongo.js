64064900e012c728a44832f04b3dd9c8fb4cb4b3
leebissessar5 <leebissessar@gmail.com>
2023-05-29 05:15:29 -0400

    complete ex3.12

---
+++ part3/phonebook/mongo.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+`mongodb+srv://fullstack:${password}@cluster0.w7eoxwq.mongodb.net/PhonebookApp?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length < 4) {
+    console.log('phonebook:')
+    Person.find({}).then(result => {
+        result.forEach(person => {
+          console.log(`${person.name} ${person.number}`)
+        })
+        mongoose.connection.close()
+    })
+}
+else {
+    if (process.argv.length < 5) {
+        console.log('please include phone number')
+        process.exit(1)
+    }
+    const person = new Person({
+        name: process.argv[3],
+        number: process.argv[4],
+    })
+    person.save().then(result => {
+        console.log(`added ${result.name} number ${result.number} to phonebook`)
+        mongoose.connection.close()
+    })
+}
