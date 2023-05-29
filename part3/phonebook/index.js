e3efc08cf6b4ff6bb8f2e825fee98aa5dcfde0e1
leebissessar5 <leebissessar@gmail.com>
2023-05-29 06:41:04 -0400

    complete ex3.15

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -70,10 +70,11 @@ app.get("/api/persons/:id", (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 const generateId = () => Math.floor(Math.random()*100) + 1
