719bb6fe2f8a5f40de82d03f3144a9cfc6c0dab0
leebissessar5 <leebissessar@gmail.com>
2023-05-31 20:42:50 -0400

    complete ex4.2

--- part4/blog-list/index.js
+++ part4/blog-list/index.js
@@ -1,17 +1,7 @@
+const app = require('./app') // the actual Express application
 const config = require('./utils/config')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const blogsRouter = require('./controllers/blogs')
-const mongoose = require('mongoose')
-
-mongoose.connect(config.MONGODB_URI)
-
-app.use(cors())
-app.use(express.json())
-
-app.use('/api/blogs', blogsRouter)
+const logger = require('./utils/logger')
 
 app.listen(config.PORT, () => {
-  console.log(`Server running on port ${config.PORT}`)
+  logger.info(`Server running on port ${config.PORT}`)
 })
