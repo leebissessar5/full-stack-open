ee094cd29b13aad8ea076dd4065837015197782c
leebissessar5 <leebissessar@gmail.com>
2023-05-31 21:11:52 -0400

    complete ex4.4

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -3,6 +3,11 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) =>
+  blogs
+    .reduce((sum, item) =>
+      item.likes + sum, 0)
+
 module.exports = {
-  dummy
+  dummy, totalLikes
 }
