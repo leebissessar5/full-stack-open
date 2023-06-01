d19d1dec460d8cd22fc569ba19317de78454ca24
leebissessar5 <leebissessar@gmail.com>
2023-05-31 20:33:02 -0400

    add lint and refactor controller

--- part4/blog-list/index.js
+++ part4/blog-list/index.js
@@ -2,32 +2,15 @@ const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
 const mongoose = require('mongoose')
-
-const Blog = require('./models/blog')
 
 mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
 app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
--- part4/blog-list/models/blog.js
+++ part4/blog-list/models/blog.js
@@ -8,11 +8,11 @@ const blogSchema = new mongoose.Schema({
 })
 
 blogSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
-  })
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
 
 module.exports = mongoose.model('Blog', blogSchema)
