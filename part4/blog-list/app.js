706ce6d823962eb9198b031116ebcdf0d7b13419
leebissessar5 <leebissessar@gmail.com>
2023-06-04 21:17:37 -0400

    complete ex4.15

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -7,6 +7,7 @@ const blogsRouter = require('./controllers/blogs')
 const mongoose = require('mongoose')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
+const usersRouter = require('./controllers/users')
 
 mongoose.set('strictQuery', false)
 
@@ -25,6 +26,7 @@ app.use(express.json())
 
 app.use(middleware.requestLogger)
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
