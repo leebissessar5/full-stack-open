1823270653b2bc4798f17e5623b5b5877eb511ec
leebissessar5 <leebissessar@gmail.com>
2023-06-08 08:51:55 -0400

    complete ex5.14

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
-//import userEvent from '@testing-library/user-event'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('renders content without url and likes by default', () => {
@@ -16,6 +16,30 @@ test('renders content without url and likes by default', () => {
 
   const div = container.querySelector('.blog')
   expect(div).toHaveTextContent(
-    `${blog.title} ${blog.author}`
+    'Go To Statement Considered Harmful Edsger W. Dijkstra'
   )
+})
+
+test('clicking the button displays url and likes', async () => {
+  const blog =   {
+    id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    user: {
+      username: 'test',
+      name: 'test'
+    }
+  }
+
+  const { container } =  render(<Blog blog={blog} />)
+
+  const user = userEvent.setup()
+  const button = screen.getByText('view')
+  await user.click(button)
+
+  const div = container.querySelector('.blog')
+  expect(div).toHaveTextContent('http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html')
+  expect(div).toHaveTextContent('likes 5')
 })
