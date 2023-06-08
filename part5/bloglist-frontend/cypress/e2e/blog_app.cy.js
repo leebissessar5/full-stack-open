a563b1dce48ab198d68ad0bb8d95136b3167440d
leebissessar5 <leebissessar@gmail.com>
2023-06-08 13:06:38 -0400

    complete ex5.18

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -15,11 +15,25 @@ describe('Blog', () => {
     cy.contains('password')
   })
 
-  // it('login form can be opened', function() {
-  //   cy.get('#username').type('johndoe')
-  //   cy.get('#password').type('weakpassword')
-  //   cy.get('#login-button').click()
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('johndoe')
+      cy.get('#password').type('weakpassword')
+      cy.get('#login-button').click()
 
-  //   cy.contains('John Doe logged in')
-  // })
+      cy.contains('John Doe logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('johndoe')
+      cy.get('#password').type('wrongpassword')
+      cy.get('#login-button').click()
+
+      cy.get('.error')
+        .should('contain', 'wrong username or password')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+        .and('have.css', 'border-style', 'solid')
+      cy.get('html').should('not.contain', 'John Doe logged in')
+    })
+  })
 })
