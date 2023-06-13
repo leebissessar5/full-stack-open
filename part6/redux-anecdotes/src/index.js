10c14036153e8ee4dbbb9d5dac577585da340364
leebissessar5 <leebissessar@gmail.com>
2023-06-13 15:50:42 -0400

    complete ex6.14

--- part6/redux-anecdotes/src/index.js
+++ part6/redux-anecdotes/src/index.js
@@ -4,9 +4,10 @@ import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 import App from './App'
 
-import anecdoteReducer from './reducers/anecdoteReducer'
+import anecdoteReducer, { appendNote } from './reducers/anecdoteReducer'
 import filterReducer from './reducers/filterReducer'
 import notificationReducer from './reducers/notificationReducer'
+import anecdoteService from './services/anecdotes'
 
 const store = configureStore(
   {
@@ -17,6 +18,13 @@ const store = configureStore(
       notification: notificationReducer
     }
   }
+)
+
+anecdoteService.getAll()
+  .then(anecdotes =>
+    anecdotes.forEach(anecdote => {
+      store.dispatch(appendNote(anecdote))
+    })
 )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,14 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit"
 
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
 const getId = () => (100000 * Math.random()).toFixed(0)
 
 const asObject = (anecdote) => {
@@ -19,11 +10,9 @@ const asObject = (anecdote) => {
   }
 }
 
-const initialState = anecdotesAtStart.map(asObject)
-
 const anecdoteSlice = createSlice({
   name: "anecdotes",
-  initialState: initialState,
+  initialState: [],
   reducers: {
     addVote(state, action) {
       const id = action.payload
@@ -34,10 +23,13 @@ const anecdoteSlice = createSlice({
     },
     createNote(state, action) {
       return [...state, asObject(action.payload)]
+    },
+    appendNote(state, action) {
+      state.push(action.payload)
     }
   }
 })
 
 
-export const { addVote, createNote } = anecdoteSlice.actions
+export const { addVote, createNote, appendNote } = anecdoteSlice.actions
 export default anecdoteSlice.reducer
