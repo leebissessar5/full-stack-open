ce777b8c7650853c92b2e04fc8f0077be0a042cf
leebissessar5 <leebissessar@gmail.com>
2023-06-16 08:48:35 -0400

    complete ex7.4

--- part7/routed-anecdotes/src/components/CreateNew.js
+++ part7/routed-anecdotes/src/components/CreateNew.js
@@ -1,16 +1,16 @@
-import { useState } from "react"
+import { useField } from "../hooks"
 
 const CreateNew = (props) => {
-    const [content, setContent] = useState('')
-    const [author, setAuthor] = useState('')
-    const [info, setInfo] = useState('')
+    const content = useField('')
+    const author = useField('')
+    const info = useField('')
 
     const handleSubmit = (e) => {
       e.preventDefault()
       props.addNew({
-        content,
-        author,
-        info,
+        content: content.value,
+        author: author.vaue,
+        info: info.value,
         votes: 0
       })
     }
@@ -21,15 +21,15 @@ const CreateNew = (props) => {
         <form onSubmit={handleSubmit}>
           <div>
             content
-            <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+            <input {...content} />
           </div>
           <div>
             author
-            <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+            <input {...author} />
           </div>
           <div>
             url for more info
-            <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+            <input {...info} />
           </div>
           <button>create</button>
         </form>
