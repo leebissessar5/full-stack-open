96715de2e34025d1a2c3725da30f1713414dd501
leebissessar5 <leebissessar@gmail.com>
2023-06-16 20:30:19 -0400

    complete ex7.14 & 15

---
+++ part7/bloglist-frontend/src/components/UserView.js
@@ -0,0 +1,37 @@
+import { useQuery } from 'react-query'
+import { useMatch } from 'react-router-dom'
+import userService from '../services/users'
+
+const UserView = () => {
+  const match = useMatch('/users/:id')
+
+  const {
+    data: users,
+    isLoading,
+    isError,
+  } = useQuery('users', userService.getAll)
+
+  if (isLoading) {
+    return <div>Loading users...</div>
+  }
+
+  if (isError) {
+    return <div>Error loading users</div>
+  }
+
+  const user = match ? users.find((user) => user.id === match.params.id) : null
+
+  return (
+    <div>
+      <h2>{user.name}</h2>
+      <h3>added blogs</h3>
+      <ul>
+        {user.blogs.map((blog) => (
+          <li key={blog.id}>{blog.title}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default UserView
