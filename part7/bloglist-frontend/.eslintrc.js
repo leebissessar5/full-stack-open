a31b3649530248499d757fc89e6d5f88314bf04b
leebissessar5 <leebissessar@gmail.com>
2023-06-17 04:18:29 -0400

    remove cypress

--- part7/bloglist-frontend/.eslintrc.js
+++ part7/bloglist-frontend/.eslintrc.js
@@ -4,7 +4,6 @@ module.exports = {
     browser: true,
     es6: true,
     'jest/globals': true,
-    'cypress/globals': true,
   },
   extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
   parserOptions: {
@@ -14,7 +13,7 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: ['react', 'jest', 'cypress', 'prettier'],
+  plugins: ['react', 'jest', 'prettier'],
   rules: {
     'linebreak-style': 0,
     quotes: ['error', 'single'],
